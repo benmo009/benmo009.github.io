97e46f0e454dbd8a53d4955dc7f4519f0d69e77d
ben <benmo009@gmail.com>
2023-01-29 01:08:13 -0800

    Add identity data file

---
+++ src/data/identity.js
@@ -0,0 +1,8 @@
+export const identity = {
+    firstName: "Ben",
+    lastName: "Mo",
+    city: "San Diego",
+    state: "CA",
+    occupation: "Electronics Engineer",
+    education: "Rensselaer Polytechnic Institute (RPI)",
+}
