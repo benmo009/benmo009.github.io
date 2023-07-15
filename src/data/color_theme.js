3cc547e7fb1d66eeacaa194178c5616e76fbe115
ben <benmo009@gmail.com>
2023-07-15 11:51:49 -0700

    Remove astro site

--- src/data/color_theme.js
+++
@@ -1,3 +0,0 @@
-export const color_theme = {
-    hyper_links: "text-sky-500",
-}
--- src/data/identity.js
+++
@@ -1,8 +0,0 @@
-export const identity = {
-    firstName: "Ben",
-    lastName: "Mo",
-    city: "San Diego",
-    state: "CA",
-    occupation: "Electronics Engineer",
-    education: "Rensselaer Polytechnic Institute (RPI)",
-}
